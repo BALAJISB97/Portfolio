1c6793e96c4433c9b1fa427edd89a77e8cf4e3a1
BALAJI <balajisb147@gmail.com>
2020-12-23 20:03:08 +0530

    Upgrade

--- homeapp/views.py
+++ homeapp/views.py
@@ -5,19 +5,19 @@ from django.conf import settings
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
 def contact(request):
-    
+    print(request)
     if request.method=="POST":
-        name,email,phone,desc=request.POST['name'],request.POST['email'],request.POST['phone'],request.POST['desc']
+        name,email,phone,desc=request.POST.get('abc','none'),request.POST['email'],request.POST['phone'],request.POST['desc']
         ins=Contact(name=name,email=email,phone=phone,desc=desc)
         ins.save()
         #construct mail message
         msg="Hey Balaji,"+name+" has enquired about you. Please check. Below are the details of the person\n"+"phone number = "+phone+"\nemail: "+email+"\n and his enquiry is "+desc
         print(msg)
         send_to_me(msg)
-    return render(request,'contact.html')
+    return render(request,'index.html')
 
 def send_to_me(msg):
     send_mail('Contact Form',msg,settings.EMAIL_HOST_USER,['balajisb147@gmail.com'],fail_silently=False)
